0d20ed1e1e8c138cc9d3cb09fbce9b17c95f4296
Super-JSB <lujessie099@gmail.com>
2026-04-16 11:22:01 -0400

    add fries instance

---
+++ Fries.java
@@ -0,0 +1,11 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+
+public class Fries extends Actor
+{
+    
+    public void act()
+    {
+        move(-3);
+    }
+}
